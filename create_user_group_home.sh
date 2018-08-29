0023a3d211670998aab43ba0184560d291a73d38
Martinus Suherman <martinus.suherman@gmail.com>
2018-08-29 18:31:52 +0700

    Simplify create user and group logic

--- create_user_group_home.sh
+++ create_user_group_home.sh
@@ -1,54 +1,26 @@
 #!/bin/sh
 
-# default PUID if not set
-DEFAULT_PGID=1000
-# default PGID if not set
-DEFAULT_PUID=1000
+# does a group with id = EGID already exist ?
+EXISTING_GROUP=$(getent group $EGID | cut -f1 -d ':')
 
-CREATE_USER=$1
-CREATE_GROUP=$2
-CREATE_HOME=$3
-
-# set default GroupID and default UserID if not already set
-if [ -z $PGID ]; then PGID=$DEFAULT_PGID; fi
-if [ -z $PUID ]; then PUID=$DEFAULT_PUID; fi
-
-
-# does a group with PGID already exist ?
-EXISTING_GROUP=$(getent group $PGID | cut -f1 -d ':')
 if [ ! -z $EXISTING_GROUP ]; then
   # change name of the existing group
-  groupmod \
-    -n $CREATE_GROUP \
-    $EXISTING_GROUP
+  groupmod -n $EGROUP $EXISTING_GROUP
 else
-  # create new group with PGID
-  addgroup \
-    -g $PGID \
-    $CREATE_GROUP  
-fi  
+  # create new group with id = EGID
+  addgroup -g $EGID $EGROUP
+fi
 
-# does a user with PUID already exist ?
-EXISTING_USER=$(getent passwd $PUID | cut -f1 -d ':')
-if [ ! -z $EXISTING__USER ]; then 
-  # change login, home, shell and primary group of the existing user
-  usermod \
-    -l $CREATE_USER \
-    -d $CREATE_HOME \
-    -s /bin/sh \
-    -g $PGID \
-    $EXISTING_USER
+# does a user with id = EUID already exist ?
+EXISTING_USER=$(getent passwd $EUID | cut -f1 -d ':')
+
+if [ ! -z $EXISTING__USER ]; then
+  # change login, home, shell (nologin) and primary group of the existing user
+  usermod -l $EUSER -d $EHOME -s /sbin/nologin -g $EGID $EXISTING_USER
 else
-  # create new user with PUID, GROUP and HOME
-  adduser \
-    -u $PUID \
-    -G $CREATE_GROUP \
-    -h $CREATE_HOME \
-    -D \
-    $CREATE_USER  
+  # create new user with id = EUID, group = EGROUP and home directory = EHOME, with nologin shell
+  adduser -s /sbin/nologin -u $EUID -G $EGROUP -h $EHOME -D $EUSER
 fi  
 
 # create user, group, and home
-chown \
-  $CREATE_USER:$CREATE_GROUP \
-  $CREATE_HOME
+chown $EUSER:$EGROUP $EHOME
