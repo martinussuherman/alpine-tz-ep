c551f1352fc00cd8b019b13e623c414998f37eee
Martinus Suherman <martinus.suherman@gmail.com>
2018-09-02 12:44:39 +0700

    Fix script syntax and logic error

--- create_user_group_home.sh
+++ create_user_group_home.sh
@@ -4,26 +4,30 @@
 # restrictive file permission (700 or 600)
 
 # does a group with name = EGROUP already exist ?
-EXISTING_GID = $( getent group $EGROUP | cut -f3 -d ':' )
+EXISTING_GID=$( getent group $EGROUP | cut -f3 -d ':' )
 
-if [ ! -z $EXISTING_GID && $EXISTING_GID != $EGID ]; then
-  # change id of the existing group
-  groupmod -g $EGID $EGROUP
+if [ ! -z $EXISTING_GID ]; then
+   if [ $EXISTING_GID != $EGID ]; then
+      # change id of the existing group
+      groupmod -g $EGID $EGROUP
+   fi
 else
-  # create new group with id = EGID
-  addgroup -g $EGID $EGROUP
+   # create new group with id = EGID
+   addgroup -g $EGID $EGROUP
 fi
 
 # does a user with name = EUSER already exist ?
-EXISTING_UID = $( getent passwd $EUSER | cut -f3 -d ':' )
+EXISTING_UID=$( getent passwd $EUSER | cut -f3 -d ':' )
 
-if [ ! -z $EXISTING_UID && $EXISTING_UID != $EUID ]; then
-  # change login, home, shell (nologin) and primary group of the existing user
-  usermod -u $EUID -d $EHOME -s /sbin/nologin -g $EGROUP $EUSER
+if [ ! -z $EXISTING_UID ]; then
+   if [ $EXISTING_UID != $EUID ]; then
+      # change login, home, shell (nologin) and primary group of the existing user
+      usermod -u $EUID -d $EHOME -s /sbin/nologin -g $EGROUP $EUSER
+   fi
 else
-  # create new user with id = EUID, group = EGROUP and home directory = EHOME,
-  # with nologin shell
-  adduser -s /sbin/nologin -u $EUID -G $EGROUP -h $EHOME -D $EUSER
+   # create new user with id = EUID, group = EGROUP and home directory = EHOME,
+   # with nologin shell
+   adduser -s /sbin/nologin -u $EUID -G $EGROUP -h $EHOME -D $EUSER
 fi
 
 # change ownership of home directory
